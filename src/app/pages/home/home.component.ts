cecde3d1e9651bef1361080c05594e87477f0462
mohamedehn <mohamed.ebarhmatin@yahoo.fr>
2024-05-30 23:05:00 +0200

    starting navbar and home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,15 +6,16 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
   imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-
-export class HomeComponent implements OnInit{
-
-  constructor() { }
+export class HomeComponent implements OnInit {
+  public name: string = 'Alisée Eggermont';
+  public introduction: string = 'Psychanalyste à Lyon';
+  public description: string =
+    'Je suis psychanalyste à Lyon. Je vous propose des consultations en cabinet pour vous aider à surmonter vos problèmes psychologiques et à retrouver un équilibre psychique. Je suis à votre écoute pour vous aider à comprendre et à résoudre vos difficultés.';
+  constructor() {}
 
   ngOnInit(): void {
     console.log('HomeComponent');
   }
-
 }
--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, CommonModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NavbarComponent {
-
+  public image = '';
+  constructor() {}
+  public navElements = [
+    { name: 'Accueil', link: '/home' },
+    { name: 'Thérapies', link: '/therapy' },
+    { name: 'Tarifs', link: '/tariff' },
+    { name: 'Contact', link: '/contact' },
+    { name: 'Votre Psychanalyste', link: '/login' },
+  ];
 }
