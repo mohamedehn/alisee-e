c7660e2d41035d50676f2015892248eb1e726534
mohamedehn <mohamed.ebarhmatin@yahoo.fr>
2024-07-03 23:14:30 +0200

    widget works with iframe. Better perf. Need more style

---
+++ src/app/shared/components/widget/widget.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-widget',
+  templateUrl: './widget.component.html',
+  styleUrls: ['./widget.component.scss'],
+  standalone: true,
+})
+export class WidgetComponent {
+  constructor() {}
+}
